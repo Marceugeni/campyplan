1a638798bf14d565e61e143caf07e812842ed9f5
Marc Eugeni <marc.gimenez@festo.com>
2022-06-14 16:30:24 +0200

    second commit

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 const express = require ('express');
+const mongoose = require('mongoose');
 const app = express();
+const path = require('path');
+const port = 3001
+
+mongoose.connect('mongodb://localhost:27017/campyplan', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on("error", function(err){
+    console.error("connection error;", err);
+});
+mongoose.connection.once("open", () => {
+    console.log("Database connected!");
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'pages'));
 
 app.get('/', (req, res) => {
-    res.send("Hello from CampyPlan!");
+    res.render('home');
 })
 
-app.listen(3000, () => {
-    console.log("Serving on port 3000")
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
